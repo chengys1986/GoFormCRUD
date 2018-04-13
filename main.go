8971d4cb9a945f123d1476326e96cda9c57e6a43
Frank Cheng <chengys1986@gmail.com>
2018-04-13 00:52:09 -0700

    test

--- main.go
+++ main.go
@@ -19,14 +19,14 @@ func main() {
 	// init config
 	configErr := config.InitConfig()
 	if configErr != nil {
-		fmt.Printf("Config Error: $s\n", configErr.Error())
+		fmt.Printf("Config Error: %s\n", configErr.Error())
 		return
 	}
 
 	// init db
 	conn, err := db.InitDB()
 	if err != nil {
-		fmt.Printf("DB Error: $s\n", err.Error())
+		fmt.Printf("DB Error: %s\n", err.Error())
 		return
 	}
 	defer db.CloseDB(conn)
